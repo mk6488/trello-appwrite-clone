d27a4e31a5ffbd5656206db05cd96a246ca57199
Mike Katholnig <mk3819@gmail.com>
2023-06-09 20:58:59 +0100

    half way marker

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
       <Header />
 
-      {/* Board */}
-      <h1>Trello 2.0 AI Clone</h1>
+      <Board />
     </main>
-  )
+  );
 }
