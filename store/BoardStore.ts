f6821766906194bd6a147d5645216f131a8f2bce
Mike Katholnig <mk3819@gmail.com>
2023-06-10 09:40:29 +0100

    persisting drag & drop in database

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,3 +1,4 @@
+import { databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTotosGroupedByColumn";
 import { create } from "zustand";
 
@@ -5,6 +6,7 @@ interface BoardState {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void;
+  updateTodoInDb: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -18,4 +20,16 @@ export const useBoardStore = create<BoardState>((set) => ({
   },
 
   setBoardState: (board) => set({ board }),
+
+  updateTodoInDb: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
 }));
